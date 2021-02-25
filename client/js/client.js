d8f29b5c10ebaa62658fd0123b2c004a6a4c0044
Feesh-X <tudoralexandru20@gmail.com>
2021-02-25 14:06:33 +0200

    sign in button

--- client/js/client.js
+++ client/js/client.js
@@ -1,5 +1,13 @@
 window.onload = function () {
 
+    function onSignIn(googleUser) {
+        var profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    }
+
     //! Constants and vars
     var game = new gameIO();
     var renderer = new game.renderer();
