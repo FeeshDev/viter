a0f64150b0d99288ec19616ef955ac3889805f98
Feesh-X <tudoralexandru20@gmail.com>
2021-02-28 20:17:49 +0200

    new key

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ app.get("/status", function (req, res) {
     res.send("ok");
 });
 
-let key = 'Ln5bdDbGJVJ8AQA3Lemgwnf9Xq7Qfjpr8Njr3ssbbmeEQqtekmLbyyjtDSQZ';
+let key = '6S6rMaqurd4epmM3rHQbspQXjKxxCYmjtySu9Mgytu6GGzQAK5wwjUZDnkX5';
 
 let playerCount = 0;
 //let servers = [];
