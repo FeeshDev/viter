f55cd07184b701ec38aa225d47f9e7bedeed3292
Feesh-X <tudoralexandru20@gmail.com>
2021-02-28 19:56:13 +0200

    obj command, changed chill to gm:1 or gm:0

--- server/server/commandHandler.js
+++ server/server/commandHandler.js
@@ -141,10 +141,21 @@ global.executeCommand = (userSelf, command, accessCode) => {
                 console.log(e);
             }
             break;
-        case "chill":
+        case "gm":
             try {
-                userSelf.invincible = !userSelf.invincible;
+                userSelf.invincible = !!parseInt(commandArray[1]);
                 userSelf.spawnProt = 0;
+            } catch (e) {
+                console.log(e);
+            }
+            break;
+        case "obj":
+            try {
+                if (commandArray[2] !== undefined) {
+                    game.create("object", [parseInt(commandArray[1]), { pos: [parseInt(commandArray[2]), parseInt(commandArray[3])] }]);
+                } else {
+                    game.create("object", [parseInt(commandArray[1]), { pos: [userSelf.body.position[0], userSelf.body.position[1]] }]);
+                }
             } catch (e) {
                 console.log(e);
             }
